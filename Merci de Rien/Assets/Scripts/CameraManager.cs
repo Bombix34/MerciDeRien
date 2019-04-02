8716dd339c089d54a7d0cb39802db89057eebce3
Bombix <hugo.bombail@wanadoo.fr>
2019-04-02 16:41:35 +0200

    Suite système camera

---
+++ Merci de Rien/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraManager : MonoBehaviour
+{
+    public CinemachineVirtualCamera baseCamera;
+    public CinemachineVirtualCamera zoomCamera;
+    public CinemachineFreeLook dialogueCamera;
+
+    public void SetNewCamera(CameraType newCameraType)
+    {
+        baseCamera.Priority = 10;
+        zoomCamera.Priority = 10;
+        dialogueCamera.Priority = 10;
+        switch(newCameraType)
+        {
+            case CameraType.Base:
+               // transform.rotation = Quaternion.Euler(45f, 0f, 0f);
+                baseCamera.Priority = 20;
+                break;
+            case CameraType.Zoom:
+               // transform.rotation = Quaternion.Euler(45f, 0f, 0f);
+                zoomCamera.Priority = 20;
+                break;
+            case CameraType.Dialogue:
+                dialogueCamera.Priority = 20;
+                break;
+        }
+    }
+    
+    public enum CameraType
+    {
+        Base,
+        Zoom,
+        Dialogue
+    }
+}
---
+++ Merci de Rien/Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour
+{
+
+    public CameraManager.CameraType cameraInZone;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag=="Player")
+        {
+            Camera.main.GetComponent<CameraManager>().SetNewCamera(cameraInZone);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Camera.main.GetComponent<CameraManager>().SetNewCamera(CameraManager.CameraType.Base);
+        }
+    }
+}
