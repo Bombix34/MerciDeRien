255cd8286682e714cc34e2ac8b0e58e8eb05d6a5
Bomb-X <bombix34@gmail.com>
2019-04-02 12:53:46 +0200

    Package personnalisation des dossiers + suite camera

---
+++ Merci de Rien/Assets/Scripts/CameraTransitionTest.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraTransitionTest : MonoBehaviour
+{
+
+    public List<CinemachineVirtualCamera> cameras;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("ok");
+            for(int i = 0; i < cameras.Count;i++)
+            {
+            }
+        }
+    }
+}
---
+++ Merci de Rien/Assets/Scripts/FX/MaterialManagement.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialManagement : MonoBehaviour
+{
+    public SkinnedMeshRenderer render;
+    Material[] objectBaseMaterials;
+
+    public Material silhouettedMaterial;
+
+    bool wasVisible = true;
+
+    void Start()
+    {
+        objectBaseMaterials = render.materials;
+    }
+
+    private void Update()
+    {
+        IsInView();
+    }
+
+    public void ResetObjectMaterials()
+    {
+        render.materials = objectBaseMaterials;
+    }
+
+    public void SetSilhouettedMaterial()
+    {
+        Material[] final = new Material[render.materials.Length];
+        for (int i = 0; i < render.materials.Length; i++)
+        {
+            final[i] = silhouettedMaterial;
+        }
+        render.materials = final;
+    }
+
+    private void IsInView()
+    {
+        Camera cam = Camera.main;
+       //Vector3 pointOnScreen = cam.WorldToScreenPoint(toCheck.GetComponentInChildren<Renderer>().bounds.center);
+        RaycastHit hit;
+
+        if (Physics.Linecast(cam.transform.position, gameObject.GetComponentInChildren<Renderer>().bounds.max, out hit))
+        {
+            if (hit.transform.name != gameObject.name)
+            {
+                if (Physics.Linecast(cam.transform.position, gameObject.GetComponentInChildren<Renderer>().bounds.center, out hit))
+                {
+                    if (hit.transform.name != gameObject.name)
+                    {
+                        hitObject = hit.transform.gameObject;
+                        if (wasVisible)
+                        {
+                            SetSilhouettedMaterial();
+                           // SetHitObjectTransparency(true);
+                            wasVisible = false;
+                        }
+                        return;
+                    }
+                }
+            }
+        }
+        if (!wasVisible)
+        {
+            ResetObjectMaterials();
+
+            //SetHitObjectTransparency(false);
+            hitObject = null;
+            wasVisible = true;
+        }
+    }
+
+    GameObject hitObject;
+
+    public void SetHitObjectTransparency(bool isTransparency)
+    {
+        //TEST DE METTRE LES AUTRS OBJETS TRANSPARENTS PLUTOT QUE LE JOUEUR EN SILHOUETTE
+        Color baseColor = hitObject.GetComponent<Renderer>().material.color;
+        Debug.Log(baseColor);
+        if(isTransparency)
+            hitObject.GetComponent<Renderer>().material.color = new Color(baseColor.r, baseColor.g,baseColor.b, 0.4f);
+        else
+            hitObject.GetComponent<Renderer>().material.color = new Color(baseColor.r, baseColor.g, baseColor.b, 1f);
+    }
+
+
+}
