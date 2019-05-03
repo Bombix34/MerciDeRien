c7a5226279c0eaf505933d1a740ce8aab61e5176
Bombix <hugo.bombail@wanadoo.fr>
2019-05-03 16:33:53 +0200

    [ADD] Prédicats [ADD] début conséquences

---
+++ Merci de Rien/Assets/Scripts/ScriptableObjects/Events/Consequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[Serializable]
+[CreateAssetMenu(menuName = "MDR/Consequence")]
+public class Consequence:ScriptableObject
+{
+
+    public ConsequenceType consequence;
+
+
+    //PNJ CHANGE BEHAVIOR
+    public PnjManager.CharacterType characterConcerned;
+    public CharacterAction actionChoice;
+
+    public enum ConsequenceType
+    {
+        PnjChangeBehavior,
+        AddDialogue
+    }
+
+    public enum CharacterAction
+    {
+        PursuitPlayer
+    }
+}
---
+++ Merci de Rien/Assets/Scripts/ScriptableObjects/Events/EventDatabase.cs
@@ -0,0 +1,200 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "MDR/EventDatabase")]
+public class EventDatabase : ScriptableObject
+{
+    /*
+     * CHARACTER______________
+     *  Artisan
+     *  Paysan
+     *  Responsable
+     *  Healer
+     *  Troubadour
+     *  Etranger   
+     */
+
+    //ATTENTION
+    //LA LISTE D'EVENT DOIT ETRE DANS LORDRE DE LENUM
+    public List<EventContainer> events;
+
+    public void ResetDatabase()
+    {
+        events = new List<EventContainer>();
+        int dataCount = (int)EventDatabase.EventType.NUMBER_OF_EVENT;
+        for (int i = 0; i < dataCount; i++)
+        {
+            events.Add(new EventContainer((EventDatabase.EventType)i, 0));
+        }
+    }
+
+    public void UpdateEvent(EventType eventType, int val)
+    {
+        events[(int)eventType].value += val;
+    }
+
+    public void UpdateCharacterEvent(EventType eventTypeGeneral, PnjManager.CharacterType character, int val)
+    {
+        if (eventTypeGeneral != EventType.violenceTotal || eventTypeGeneral != EventType.conversationTotal || eventTypeGeneral != EventType.brokeObjectsTotal
+            || eventTypeGeneral != EventType.stealedObjectsTotal || eventTypeGeneral != EventType.questTotal)
+            return;
+        events[(int)eventTypeGeneral].value += val;
+        //ATTENTION : LES EVENT PERSO DOIVENT TOUJOURS ETRE DANS LORDRE ART - PAYS - RESP - HEAL - TROUB - ETRANG
+        int indexEventCharacter = (int)eventTypeGeneral + (int)character + 1;
+        events[indexEventCharacter].value += val;
+
+        UpdateCharactersMet(eventTypeGeneral);
+    }
+
+    public EventContainer GetEvent(EventType eventType)
+    {
+        return events[(int)eventType];
+    }
+
+    public void UpdateCharactersMet(EventType eventType)
+    {
+        if (eventType != EventType.conversationTotal)
+            return;
+        EventContainer characterMet = GetEvent(EventType.charactersMet);
+        characterMet.value = 0;
+        if (GetEvent(EventType.conversationWithHealer).value > 0)
+            characterMet.value++;
+        else if (GetEvent(EventType.conversationWithPaysan).value > 0)
+            characterMet.value++;
+        else if (GetEvent(EventType.conversationWithArtisan).value > 0)
+            characterMet.value++;
+        else if (GetEvent(EventType.conversationWithEtranger).value > 0)
+            characterMet.value++;
+        else if (GetEvent(EventType.conversationWithTroubadour).value > 0)
+            characterMet.value++;
+        else if (GetEvent(EventType.conversationWithResponsable).value > 0)
+            characterMet.value++;
+    }
+
+    public enum EventType
+    {
+        violenceTotal,
+        violenceAgainstArtisan,
+        violenceAgainstPaysan,
+        violenceAgainstResponsable,
+        violenceAgainstHealer,
+        violenceAgainstTroubadour,
+        violenceAgainstEtranger,
+
+        distanceDone,
+        zoneDiscovered,
+        timePassed,
+
+        conversationTotal,
+        conversationWithArtisan,
+        conversationWithPaysan,
+        conversationWithResponsable,
+        conversationWithHealer,
+        conversationWithTroubadour,
+        conversationWithEtranger,
+        charactersMet,                  //DONE
+
+        brokeObjectsTotal,
+        brokeObjectsArtisan,
+        brokeObjectsPaysan,
+        brokeObjectsResponsable,
+        brokeObjectsHealer,
+        brokeObjectsTroubadour,
+        brokeObjectsEtranger,
+        floreBrokeObjects,
+
+        stealedObjectsTotal,
+        stealedObjectsArtisan,
+        stealedObjectsPaysan,
+        stealedObjectsResponsable,
+        stealedObjectsHealer,
+        stealedObjectsTroubadour,
+        stealedObjectsEtranger,
+
+        questTotal,
+        questArtisan,
+        questPaysan,
+        questResponsable,
+        questHealer,
+        questTroubadour,
+        questEtranger,
+        currentQuest,
+
+        NUMBER_OF_EVENT
+    }
+
+}
+
+
+/*
+ //TOD0___________________________________
+    [Header("VIOLENCE AGAINST CHARACTERS")]//_________________________________________
+    public int violenceTotal = 0;
+    [Space]
+    public int violenceAgainstArtisan = 0;
+    public int violenceAgainstPaysan = 0;
+    public int violenceAgainstResponsable = 0;
+    public int violenceAgainstHealer = 0;
+    public int violenceAgainstTroubadour = 0;
+    public int violenceAgainstEtranger = 0;
+
+    [Space]
+
+    [Header("ZONE, DISTANCE AND TIME")]//_________________________________________
+    public int distanceDone = 0;
+    public int zoneDiscovered = 0;
+    public int timePassed = 0;
+
+    [Space]
+
+    [Header("TALKING EVENTS")]//_________________________________________
+    public int conversationTotal = 0;
+    [Space]
+    public int conversationWithArtisan = 0;
+    public int conversationWithPaysan = 0;
+    public int conversationWithResponsable = 0;
+    public int conversationWithHealer = 0;
+    public int conversationWithTroubadour = 0;
+    public int conversationWithEtranger = 0;
+    [Space]
+    public int charactersMet = 0;
+
+    [Space]
+
+    [Header("BREAK OBJECTS")]//_________________________________________
+    public int brokeObjectsTotal = 0;
+    [Space]
+    public int brokeObjectsArtisan = 0;
+    public int brokeObjectsPaysan = 0;
+    public int brokeObjectsResponsable = 0;
+    public int brokeObjectsHealer = 0;
+    public int brokeObjectsTroubadour = 0;
+    public int brokeObjectsEtranger = 0;
+    [Space]
+    public int floreBrokeObjects = 0;
+
+    [Space]
+
+    [Header("STEAL OBJECTS")]//_________________________________________
+    public int stealedObjectsTotal = 0;
+    [Space]
+    public int stealedObjectsArtisan = 0;
+    public int stealedObjectsPaysan = 0;
+    public int stealedObjectsResponsable = 0;
+    public int stealedObjectsHealer = 0;
+    public int stealedObjectsTroubadour = 0;
+    public int stealedObjectsEtranger = 0;
+
+    [Header("QUEST")]//_________________________________________
+    public int questTotal = 0;
+    [Space]
+    public int questArtisan = 0;
+    public int questPaysan = 0;
+    public int questResponsable = 0;
+    public int questHealer = 0;
+    public int questTroubadour = 0;
+    public int questEtranger = 0;
+    [Space]
+    public int currentQuest = 0;
+*/
