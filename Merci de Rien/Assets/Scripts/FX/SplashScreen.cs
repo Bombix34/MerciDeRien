c6e2ed9a1fa49abd8d496469bed228b2fef706d7
Bomb-X <bombix34@gmail.com>
2019-06-03 23:11:36 +0200

    [ADD] splash screen + loading [DEBUG] liste en description
    - debug : object qui tourne yolo quand on les portes
    - gd : enlever taper les pnjs de bad things
    - debug : rotation du perso a la fin du jeu 
    - FX : cycle jour/nuit setup

---
+++ Merci de Rien/Assets/Scripts/FX/SplashScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreen : MonoBehaviour
+{
+    public float splashTime = 3f;
+    public string nextScene;
+
+    void Update()
+    {
+        splashTime -= Time.deltaTime;
+        if(splashTime<=0)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}
