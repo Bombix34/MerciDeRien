1d7b1358fb94d463c3f40b158c3cfe7d1feef863
Bombix <hugo.bombail@wanadoo.fr>
2019-06-04 19:46:35 +0200

    [UPDATE] fin de la cérémonie + ty for playing + retour menu

---
+++ Merci de Rien/Assets/Scripts/EndText.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndText : MonoBehaviour
+{
+    [SerializeField]
+    Dialogue textThanks;
+    Text zoneText;
+
+    [SerializeField]
+    SettingsManager settings;
+
+    void Start()
+    {
+        zoneText = GetComponent<Text>();
+        if(settings.currentLanguage==SettingsManager.Language.francais)
+        {
+            zoneText.text = textThanks.frenchSentences[0];
+        }
+        else
+        {
+            zoneText.text = textThanks.englishSentences[0];
+        }
+    }
+}
---
+++ Merci de Rien/Assets/Scripts/EndTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndTrigger : MonoBehaviour
+{
+    public float waitTime = 5f;
+    float timeToChange = 5f;
+
+    [SerializeField]
+    GameObject endPanel;
+
+    void Start()
+    {
+        endPanel.SetActive(false);
+    }
+    
+    void Update()
+    {
+        waitTime -= Time.deltaTime;
+        if(waitTime<=0)
+        {
+            endPanel.SetActive(true);
+            timeToChange -= Time.deltaTime;
+            if(timeToChange<=0)
+            {
+                SceneManager.LoadScene("SplashScreen");
+            }
+        }
+    }
+}
