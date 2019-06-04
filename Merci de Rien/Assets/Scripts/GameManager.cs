17fbc66a228f9cfa8d6e79c37ca278f0a50328ce
Bombix <hugo.bombail@wanadoo.fr>
2019-06-04 13:23:06 +0200

    [ADD] debut scene ceremonie

--- Merci de Rien/Assets/Scripts/GameManager.cs
+++ Merci de Rien/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
@@ -44,6 +45,8 @@ public class GameManager : Singleton<GameManager>
         yield return new WaitForSeconds(3f);
         cameraManager.SetDialogueCamera(orb);
         FlashFXManager.Instance.Fondu(Color.black);
+        yield return new WaitForSeconds(3f);
+        SceneManager.LoadScene("Ceremonie");
     }
 
     public void AddToHistoric(Dialogue newDialogue)
